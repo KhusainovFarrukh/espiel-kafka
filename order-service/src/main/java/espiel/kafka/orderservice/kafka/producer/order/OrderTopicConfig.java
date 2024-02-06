497332e6e165a25f0414284f6c9c20bb069a80f7
f_khusainov <f.khusainov@uzinfocom.uz>
2024-02-07 01:07:53 +0500

    Migrated topic name config from Java to YAML

--- order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderTopicConfig.java
+++ order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderTopicConfig.java
@@ -1,17 +1,19 @@
 package espiel.kafka.orderservice.kafka.producer.order;
 
-import static espiel.kafka.orderservice.kafka.producer.order.OrderProducer.TOPIC_ACTIVE_ORDERS_COUNT;
-
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OrderTopicConfig {
 
+  @Value("${kafka.producer.active-orders-count.topic}")
+  private String topicActiveOrdersCount;
+
   @Bean
   public NewTopic orderTopic() {
-    return new NewTopic(TOPIC_ACTIVE_ORDERS_COUNT, 1, (short) 1);
+    return new NewTopic(topicActiveOrdersCount, 1, (short) 1);
   }
 
 }
