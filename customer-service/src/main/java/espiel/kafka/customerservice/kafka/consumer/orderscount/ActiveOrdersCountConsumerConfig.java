f7b6e736a738129a133c38725fac6576bfead01a
f_khusainov <f.khusainov@uzinfocom.uz>
2024-02-08 15:43:58 +0500

    Used auth (Kafka) in consumer side

--- customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumerConfig.java
+++ customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumerConfig.java
@@ -2,7 +2,11 @@ package espiel.kafka.customerservice.kafka.consumer.orderscount;
 
 import espiel.kafka.customerservice.kafka.consumer.orderscount.model.ActiveOrdersCountMessage;
 import java.util.Map;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +22,14 @@ public class ActiveOrdersCountConsumerConfig {
   @Value("${kafka.consumer.active-orders-count.bootstrap-servers}")
   private String bootstrapServers;
 
+  @Value("${kafka.consumer.active-orders-count.username}")
+  private String username;
+
+  @Value("${kafka.consumer.active-orders-count.password}")
+  private String password;
+
   private static final String TYPE_KEY_ACTIVE_ORDERS_COUNT = "activeOrdersCount";
+  public static final String SASL_MECHANISM_PLAIN = "PLAIN";
 
   @Bean
   public ConcurrentKafkaListenerContainerFactory<String, ActiveOrdersCountMessage> kafkaListenerContainerFactory() {
@@ -35,6 +46,12 @@ public class ActiveOrdersCountConsumerConfig {
   private Map<String, Object> consumerConfigs() {
     return Map.of(
         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
+        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
+        SaslConfigs.SASL_JAAS_CONFIG, String.format(
+            "%s required username=\"%s\" password=\"%s\";",
+            PlainLoginModule.class.getName(), username, password
+        ),
         ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
         JsonDeserializer.TRUSTED_PACKAGES, "*",
