10f6377d48598fd8164e1a4f3c9feaf87bf31455
f_khusainov <f.khusainov@uzinfocom.uz>
2024-02-09 11:32:59 +0500

    Implemented Request-Reply mechanism in producer side

--- order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderProducerConfig.java
+++ order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderProducerConfig.java
@@ -3,17 +3,23 @@ package espiel.kafka.orderservice.kafka.producer.order;
 import espiel.kafka.orderservice.kafka.producer.order.model.ActiveOrdersCountMessage;
 import java.util.Map;
 import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.security.plain.PlainLoginModule;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
@@ -28,19 +34,40 @@ public class OrderProducerConfig {
   @Value("${kafka.producer.active-orders-count.password}")
   private String password;
 
+  @Value("${kafka.producer.active-orders-count.replies-topic}")
+  private String repliesTopic;
+
+  @Value("${kafka.producer.active-orders-count.replies-group-id}")
+  private String repliesGroupId;
+
   private static final String TYPE_KEY_ACTIVE_ORDERS_COUNT = "activeOrdersCount";
   public static final String SASL_MECHANISM_PLAIN = "PLAIN";
 
   @Bean
-  public KafkaTemplate<String, ActiveOrdersCountMessage> kafkaTemplate(
-      ProducerFactory<String, ActiveOrdersCountMessage> producerFactory
+  public ReplyingKafkaTemplate<String, ActiveOrdersCountMessage, String> kafkaTemplate(
+      ProducerFactory<String, ActiveOrdersCountMessage> producerFactory,
+      KafkaMessageListenerContainer<String, String> repliesContainer
   ) {
-    return new KafkaTemplate<>(producerFactory);
+    return new ReplyingKafkaTemplate<>(producerFactory, repliesContainer);
   }
 
   @Bean
   public ProducerFactory<String, ActiveOrdersCountMessage> producerFactory() {
     return new DefaultKafkaProducerFactory<>(producerConfigs());
+  }
+
+  @Bean
+  public KafkaMessageListenerContainer<String, String> repliesContainer(
+      ConsumerFactory<String, String> replyConsumerFactory
+  ) {
+    var containerProperties = new ContainerProperties(repliesTopic);
+    containerProperties.setGroupId(repliesGroupId);
+    return new KafkaMessageListenerContainer<>(replyConsumerFactory, containerProperties);
+  }
+
+  @Bean
+  public ConsumerFactory<String, String> replyConsumerFactory() {
+    return new DefaultKafkaConsumerFactory<>(replyConsumerConfigs());
   }
 
   private Map<String, Object> producerConfigs() {
@@ -56,6 +83,20 @@ public class OrderProducerConfig {
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
         JsonSerializer.TYPE_MAPPINGS,
         TYPE_KEY_ACTIVE_ORDERS_COUNT + ":" + ActiveOrdersCountMessage.class.getName()
+    );
+  }
+
+  private Map<String, Object> replyConsumerConfigs() {
+    return Map.of(
+        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
+        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
+        SaslConfigs.SASL_JAAS_CONFIG, String.format(
+            "%s required username=\"%s\" password=\"%s\";",
+            PlainLoginModule.class.getName(), username, password
+        ),
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class
     );
   }
 
