b37c1f6dc6527e15352f95f8656252c7c07e6bc8
f_khusainov <f.khusainov@uzinfocom.uz>
2024-02-09 11:35:08 +0500

    Implemented Request-Reply mechanism in consumer side

--- customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumer.java
+++ customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumer.java
@@ -2,12 +2,16 @@ package espiel.kafka.customerservice.kafka.consumer.orderscount;
 
 import espiel.kafka.customerservice.customer.CustomerService;
 import espiel.kafka.customerservice.kafka.consumer.orderscount.model.ActiveOrdersCountMessage;
+import java.util.Arrays;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.AbstractConsumerSeekAware;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,8 +27,12 @@ public class ActiveOrdersCountConsumer extends AbstractConsumerSeekAware {
       topics = "${kafka.consumer.active-orders-count.topic}",
       groupId = "${kafka.consumer.active-orders-count.group-id}"
   )
-  public void consume(ActiveOrdersCountMessage message) {
-    customerService.updateActiveOrdersCount(message);
+  @SendTo
+  public String consume(ConsumerRecord<String, ActiveOrdersCountMessage> message) {
+    customerService.updateActiveOrdersCount(message.value());
+    return Arrays.toString(
+        message.headers().headers(KafkaHeaders.CORRELATION_ID).iterator().next().value()
+    );
   }
 
   @Override
