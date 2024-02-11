22ffe1b9a9320c638de70df32bdd1a485e96b26d
f_khusainov <f.khusainov@uzinfocom.uz>
2024-02-11 21:37:21 +0500

    Implemented replying when using Batch listener

--- customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumer.java
+++ customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumer.java
@@ -2,13 +2,20 @@ package espiel.kafka.customerservice.kafka.consumer.orderscount;
 
 import espiel.kafka.customerservice.customer.CustomerService;
 import espiel.kafka.customerservice.kafka.consumer.orderscount.model.ActiveOrdersCountMessage;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.header.Headers;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.AbstractConsumerSeekAware;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -42,8 +49,26 @@ public class ActiveOrdersCountConsumer extends AbstractConsumerSeekAware {
       topics = "${kafka.consumer.active-orders-count.topic}",
       groupId = "${kafka.consumer.active-orders-count.group-id}"
   )
-  public void consume(List<ActiveOrdersCountMessage> messages) {
-    customerService.updateActiveOrdersCounts(messages);
+  @SendTo
+  public Collection<Message<String>> consume(
+      List<ConsumerRecord<String, ActiveOrdersCountMessage>> messages
+  ) {
+    customerService.updateActiveOrdersCounts(
+        messages.stream().map(ConsumerRecord::value).toList()
+    );
+
+    return messages.stream()
+        .map(message -> {
+          var correlationId = getHeaderAsString(message.headers(), KafkaHeaders.CORRELATION_ID);
+          var replyTopic = getHeaderAsString(message.headers(), KafkaHeaders.REPLY_TOPIC);
+
+          return MessageBuilder
+              .withPayload(correlationId)
+              .setHeader(KafkaHeaders.TOPIC, replyTopic)
+              .setHeader(KafkaHeaders.CORRELATION_ID, correlationId)
+              .build();
+        })
+        .toList();
   }
 
   @Override
@@ -55,6 +80,19 @@ public class ActiveOrdersCountConsumer extends AbstractConsumerSeekAware {
     if (Boolean.TRUE.equals(consumeFromBeginning)) {
       seekToBeginning();
     }
+  }
+
+  private static String getHeaderAsString(
+      Headers headers,
+      String headerName
+  ) {
+    return new String(
+        headers
+            .headers(headerName)
+            .iterator()
+            .next()
+            .value()
+    );
   }
 
 }
