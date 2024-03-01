a1e8baa8595de4750b44be25c20934269de8ebab
f_khusainov <f.khusainov@uzinfocom.uz>
2024-03-01 18:08:39 +0500

    Implemented using Kafka without Zookeeper (KRaft mode)

--- customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumerConfig.java
+++ customer-service/src/main/java/espiel/kafka/customerservice/kafka/consumer/orderscount/ActiveOrdersCountConsumerConfig.java
@@ -5,9 +5,7 @@ import java.util.Map;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
-import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,14 +25,7 @@ public class ActiveOrdersCountConsumerConfig {
   @Value("${kafka.consumer.active-orders-count.bootstrap-servers}")
   private String bootstrapServers;
 
-  @Value("${kafka.consumer.active-orders-count.username}")
-  private String username;
-
-  @Value("${kafka.consumer.active-orders-count.password}")
-  private String password;
-
   private static final String TYPE_KEY_ACTIVE_ORDERS_COUNT = "activeOrdersCount";
-  public static final String SASL_MECHANISM_PLAIN = "PLAIN";
 
   @Bean
   public ConcurrentKafkaListenerContainerFactory<String, ActiveOrdersCountMessage> kafkaListenerContainerFactory(
@@ -67,12 +58,7 @@ public class ActiveOrdersCountConsumerConfig {
   private Map<String, Object> consumerConfigs() {
     return Map.of(
         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
-        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
-        SaslConfigs.SASL_JAAS_CONFIG, String.format(
-            "%s required username=\"%s\" password=\"%s\";",
-            PlainLoginModule.class.getName(), username, password
-        ),
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.PLAINTEXT.name,
         ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
         JsonDeserializer.TRUSTED_PACKAGES, "*",
@@ -84,12 +70,7 @@ public class ActiveOrdersCountConsumerConfig {
   private Map<String, Object> replyProducerConfigs() {
     return Map.of(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
-        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
-        SaslConfigs.SASL_JAAS_CONFIG, String.format(
-            "%s required username=\"%s\" password=\"%s\";",
-            PlainLoginModule.class.getName(), username, password
-        ),
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.PLAINTEXT.name,
         ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
     );
--- order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderProducerConfig.java
+++ order-service/src/main/java/espiel/kafka/orderservice/kafka/producer/order/OrderProducerConfig.java
@@ -6,9 +6,7 @@ import java.util.Map;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
-import org.apache.kafka.common.security.plain.PlainLoginModule;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,12 +27,6 @@ public class OrderProducerConfig {
   @Value("${kafka.producer.active-orders-count.bootstrap-servers}")
   private String bootstrapServers;
 
-  @Value("${kafka.producer.active-orders-count.username}")
-  private String username;
-
-  @Value("${kafka.producer.active-orders-count.password}")
-  private String password;
-
   @Value("${kafka.producer.active-orders-count.replies-topic}")
   private String repliesTopic;
 
@@ -45,7 +37,6 @@ public class OrderProducerConfig {
   private Long repliesTimeoutDays;
 
   private static final String TYPE_KEY_ACTIVE_ORDERS_COUNT = "activeOrdersCount";
-  public static final String SASL_MECHANISM_PLAIN = "PLAIN";
 
   @Bean
   public ReplyingKafkaTemplate<String, ActiveOrdersCountMessage, String> kafkaTemplate(
@@ -80,12 +71,7 @@ public class OrderProducerConfig {
   private Map<String, Object> producerConfigs() {
     return Map.of(
         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
-        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
-        SaslConfigs.SASL_JAAS_CONFIG, String.format(
-            "%s required username=\"%s\" password=\"%s\";",
-            PlainLoginModule.class.getName(), username, password
-        ),
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.PLAINTEXT.name,
         ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
         ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
         JsonSerializer.TYPE_MAPPINGS,
@@ -96,12 +82,7 @@ public class OrderProducerConfig {
   private Map<String, Object> replyConsumerConfigs() {
     return Map.of(
         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_PLAINTEXT.name,
-        SaslConfigs.SASL_MECHANISM, SASL_MECHANISM_PLAIN,
-        SaslConfigs.SASL_JAAS_CONFIG, String.format(
-            "%s required username=\"%s\" password=\"%s\";",
-            PlainLoginModule.class.getName(), username, password
-        ),
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.PLAINTEXT.name,
         ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class
     );
